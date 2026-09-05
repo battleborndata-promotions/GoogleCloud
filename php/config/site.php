a4ee0653d3bd634004bda6e6e5aad5cb7ebdc053
battleborndata-promotions <battleborndata@gmail.com>
2026-09-04 19:54:16 -0700

    Add site configuration for Vassar Lounge

---
+++ php/config/site.php
@@ -0,0 +1,23 @@
+<?php
+
+$siteConfig = [
+    'business_name' => 'Vassar Lounge',
+
+    'tagline' => 'Your Neighborhood Hangout',
+
+    'address' => '1545 Vassar St, Reno, NV 89502',
+
+    'phone' => '',
+
+    'email' => '',
+
+    'instagram_url' => '',
+
+    'facebook_url' => '',
+
+    'page_title_suffix' => 'Vassar Lounge',
+
+    'hero_heading' => 'Your Neighborhood Hangout',
+
+    'hero_text' => 'Drinks, events, games, and good company in Reno.'
+];
